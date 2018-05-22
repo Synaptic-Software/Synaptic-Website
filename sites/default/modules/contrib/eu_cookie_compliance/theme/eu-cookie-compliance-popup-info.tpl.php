0317d7b32db2dba3da56201b03e79c72f4b6e8e7
Synaptic Infrastructure <DL-CFSL-Infrastructure@capita.co.uk>
2018-05-22 11:54:22 +0100

    copy of live site following GIT add and commit

--- sites/default/modules/contrib/eu_cookie_compliance/theme/eu-cookie-compliance-popup-info.tpl.php
+++ sites/default/modules/contrib/eu_cookie_compliance/theme/eu-cookie-compliance-popup-info.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * This is a template file for a pop-up prompting user to give their consent for
@@ -11,9 +12,9 @@
  * find-more-button  - link to an information page
  *
  * Variables available:
- * - $message:  Contains the text that will be display whithin the pop-up
+ * - $message:  Contains the text that will be display within the pop-up
  * - $agree_button: Contains agree button title
- * - $disagree_button: Contains disagree button title
+ * - $disagree_button: Contains Cookie policy button title
  */
 ?>
 <div>
